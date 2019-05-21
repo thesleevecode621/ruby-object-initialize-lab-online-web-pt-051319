e8afad538c5b4367368c5481517b7aea9094bdc5
boruch d <dubywider@gmail.com>
2019-05-21 17:04:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,19 @@
+
 class Dog
-  
-  def initialize(breed)
-  
-    @breed = breed
-    
+  def initialize (dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
   end
- def name=(name)
-   @name=name 
- end 
- def name 
-   @name 
- end 
-  def breed=(breed)
-    @breed = breed
+
+  def name=(dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
   end
- 
+
+  def name
+    @name
+  end
   def breed
     @breed
-  end
-end
+  end 
+end 
